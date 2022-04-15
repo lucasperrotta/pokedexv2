6c57e1b527e5d52c31d8e84cbf1f9eea11080e11
Lucas Perrotta Barbosa <lucas.perrotta@k4d.com.br>
2022-04-15 16:40:29 -0300

    responsive fixes

--- pages/index.js
+++ pages/index.js
@@ -7,12 +7,21 @@ import Link from '../src/Link';
 import Copyright from '../src/Copyright';
 import { boxSizing, height } from '@mui/system';
  */
+
+const pikachu = {
+  id: "001",
+  name: 'Pikachu',
+  types: ['Electric'],
+}
+const pokemons = [pikachu, pikachu, pikachu]
+
 const Search = () => (
   <Box className='search' component='input' sx={{
     height: "40px",
     flexGrow: "1",
-    maxWidth: '300px',
-    minWidth: '1px',
+    display: "flex",
+    maxWidth: "500px",
+    minWidth: "100px",
     border: 'none',
     borderRadius: '10px',
     bgcolor: 'red.navsearchbg',
@@ -56,48 +65,65 @@ const Header = ({ children }) => (
     display: 'flex',
     alignItems: 'center',
     px: "20px",
-    py: "30px"
+    py: "30px",
+    justifyContent: 'space-between',
   }}>
-
-    <Box sx={{ display: 'flex', flexGrow: 3 }}>
+    <Box sx={{ display: 'flex', flexGrow: 3, mr: 1 }}>
       <BlueBall />
       <Ball color="red.ball" />
       <Ball color="yellow.ball" />
       <Ball color="green.ball" />
     </Box>
     <Search />
-
     {children}
   </Box>
 )
 
-const Pokebox = ({ id }) => (
-  <Box sx={{
-    bgcolor: 'secondary.light',
-    width: 1,
-    maxHeight: '150px',
-    display: 'flex',
-    alignItems: 'center',
-    p: 2,
-    mt: 1,
-    flexGrow: 1,
-    alignSelf: 'start',
-    borderRadius: "10px"
-  }} >
-    <Typography variant='h6' sx={{ display: "flex" }}>
-      Pokemon {id}
-    </Typography>
-  </Box>
-)
+const Pokebox = ({ pokemon }) => {
+  const { id, name, types } = pokemon
+  return (
+    < Box sx={{
+      bgcolor: 'electric',
+      width: 1,
+      maxHeight: '110px',
+      height: '110px',
+      minHeight: '1px',
+      display: 'flex',
+      p: 1,
+      mt: 1,
+      flexGrow: 1,
+      alignSelf: 'start',
+      borderRadius: "10px",
+      flexFlow: 'row wrap',
+    }} >
+      <Box component='span' sx={{
+        height: 1,
+        display: 'flex',
+        flexFlow: 'column wrap',
+        justifyContent: 'space-between',
+        flexGrow: 1,
+        alignSelf: 'start',
+        borderRadius: "10px",
+
+      }}>
+        <Typography variant="subtitle1">#{id} </Typography>
+        <Typography variant="h5" sx={{ fontSize: '1.4rem', fontFamily: 'Varela Round', fontWeight: "bold" }}>{name} <br /></Typography>
+        <Typography sx={{ fontSize: '0.9rem', }}>{types.map(type => type).join(', ')}<br /></Typography>
+
+      </Box>
+    </Box >
+  )
+}
 const Content = ({ children }) => (
   <Box sx={{
-    bgcolor: 'white.main',
+    bgcolor: 'white',
     width: 1,
+
     flexGrow: 1,
-    px: 3,
-    py: 3,
+
+    p: 3,
     display: 'flex',
-    flexFlow: 'column wrap',
+    flexFlow: 'column nowrap',
   }}>
     {children}
   </Box>
@@ -106,10 +132,10 @@ const Footer = ({ children }) => (
   <Box sx={{
     bgcolor: 'red.footer',
     width: 1,
-    height: '120px',
+    minHeight: '120px',
     display: 'flex',
   }}>
-
+    a
     {children}
   </Box>
 )
@@ -117,12 +143,14 @@ const Footer = ({ children }) => (
 const Screen = ({ children }) => (
   < Box sx={{
     display: 'flex',
-    flexFlow: 'column wrap',
     height: '100vh',
+    flexFlow: 'column nowrap',
   }}>
     {children}
   </Box>
 );
+
+
 
 const Index = () => (
   <Screen>
@@ -132,6 +160,7 @@ const Index = () => (
       <Typography variant='h3' component='div' sx={{
         fontFamily: 'Varela Round',
         fontSize: '3rem',
+        fontWeight: 'bold',
       }}>
         Pokedex
       </Typography>
@@ -148,10 +177,11 @@ const Index = () => (
         flexFlow: 'column wrap',
         justifyContent: 'space-around',
         flexGrow: 1,
+        my: 3
       }}>
-        <Pokebox id={1} />
-        <Pokebox id={2} />
-        <Pokebox id={3} />
+        <Pokebox pokemon={pikachu} />
+        <Pokebox pokemon={pikachu} />
+        <Pokebox pokemon={pikachu} />
       </Box>
 
     </Content>
